d1ed3da151fc23100c9b9e5e3fb02dc15dd48aa1
Justin Tormey <justin@blockchain.com>
2017-08-30 16:11:11 -0400

    add counter package

---
+++ packages/counter/index.js
@@ -0,0 +1,16 @@
+
+class Counter {
+  constructor (init) {
+    this.value = init
+  }
+
+  increment () {
+    this.value++
+  }
+
+  decrement () {
+    this.value++
+  }
+}
+
+export default Counter
